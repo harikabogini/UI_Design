cb329c801e8b2a8791bd854774a1b749b9a37721
harikabogini <harikabogini@gmail.com>
2025-03-06 10:13:32 +0530

    Update P2.dart

--- LAB4/P2.dart
+++ LAB4/P2.dart
@@ -1,1 +1,19 @@
+import 'dart:io';
+void main()
+{
+	print("enter the a value");
+	var a =int.parse( stdin.readLineSync()!);
+	print("enter the b value");
+	var b = int.parse(stdin.readLineSync()!);
+	
+	print(sum(a,b));
+	print(sub(a,b));
+	print(div(a,b));
+	print(mul(a,b));
+
+}
+int sum(a,b) => a+b;
+int sub(a,b) => a-b;
+int div(a,b) => a~/b;
+int mul(a,b) => a*b;
 
